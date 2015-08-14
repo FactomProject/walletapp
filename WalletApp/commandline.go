a263f311a15f8448e19360d338ec5e885650f766
ThePiachu <ThePiachu@gmail.com>
2015-08-14 16:44:50 -0700

    Go fmt

--- WalletApp/commandline.go
+++ WalletApp/commandline.go
@@ -5,35 +5,33 @@
 package main
 
 import (
-    "os"
+	"bufio"
 	"fmt"
-    "time"
-    "bufio"
-    "strings"
-    fct "github.com/FactomProject/factoid"
-   // "golang.org/x/crypto/ssh/terminal"
+	fct "github.com/FactomProject/factoid"
+	"os"
+	"strings"
+	"time"
+	// "golang.org/x/crypto/ssh/terminal"
 )
+
 var _ = fmt.Println
 var _ fct.Transaction
-var _ = time.Now 
- 
+var _ = time.Now
+
 func main() {
-    state := NewState()
-    r := bufio.NewReader(os.Stdin)
-    for {
-        fmt.Print(" Factom Wallet$ ")
-        line,_,_ := r.ReadLine()
-        args := strings.Fields(string(line))    
-        err := state.Execute(args)
-        if err != nil {
-            fmt.Println(err)
-            c,_ := state.GetCommand(args)
-            if c != nil {
-                fmt.Println(c.ShortHelp())
-            }
-        }
-    }
+	state := NewState()
+	r := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print(" Factom Wallet$ ")
+		line, _, _ := r.ReadLine()
+		args := strings.Fields(string(line))
+		err := state.Execute(args)
+		if err != nil {
+			fmt.Println(err)
+			c, _ := state.GetCommand(args)
+			if c != nil {
+				fmt.Println(c.ShortHelp())
+			}
+		}
+	}
 }
-
-
-
--- handlers/generateaddress.go
+++ handlers/generateaddress.go
@@ -5,42 +5,42 @@
 package handlers
 
 import (
-    "github.com/hoisie/web"
-    fct "github.com/FactomProject/factoid"
+	fct "github.com/FactomProject/factoid"
+	"github.com/hoisie/web"
 
-    "github.com/FactomProject/fctwallet/Wallet"
+	"github.com/FactomProject/fctwallet/Wallet"
 )
 
 var _ = fct.Address{}
 
-func  HandleFactoidGenerateAddress(ctx *web.Context, name string) {
-    msg, ok := ValidateKey(name) 
-    if !ok {
-        reportResults(ctx, msg, false)
-        return
-    }
+func HandleFactoidGenerateAddress(ctx *web.Context, name string) {
+	msg, ok := ValidateKey(name)
+	if !ok {
+		reportResults(ctx, msg, false)
+		return
+	}
 
-    adrstr, err:=Wallet.GenerateAddressString(name)
-    if err!=nil {
-        reportResults(ctx, err.Error(), false)
-        return
-    }
-        
-    reportResults(ctx,adrstr,true)
+	adrstr, err := Wallet.GenerateAddressString(name)
+	if err != nil {
+		reportResults(ctx, err.Error(), false)
+		return
+	}
+
+	reportResults(ctx, adrstr, true)
 }
 
-func  HandleFactoidGenerateECAddress(ctx *web.Context, name string) {
-    msg, ok := ValidateKey(name) 
-    if !ok {
-        reportResults(ctx, msg, false)
-        return
-    }
-    
-    adrstr, err := Wallet.GenerateECAddressString(name)
-    if err!=nil {
-        reportResults(ctx, err.Error(), false)
-        return
-    }
-    
-    reportResults(ctx, adrstr, true)
+func HandleFactoidGenerateECAddress(ctx *web.Context, name string) {
+	msg, ok := ValidateKey(name)
+	if !ok {
+		reportResults(ctx, msg, false)
+		return
+	}
+
+	adrstr, err := Wallet.GenerateECAddressString(name)
+	if err != nil {
+		reportResults(ctx, err.Error(), false)
+		return
+	}
+
+	reportResults(ctx, adrstr, true)
 }
