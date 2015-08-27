908f05c0ad5d96a369a42ffc13954b5db83129f0
ThePiachu <ThePiachu@gmail.com>
2015-08-27 15:48:14 -0700

    Added some logic for private key importing.

--- handlers/generateaddress.go
+++ handlers/generateaddress.go
@@ -6,9 +6,9 @@ package handlers
 
 import (
 	fct "github.com/FactomProject/factoid"
-	"github.com/hoisie/web"
-	"github.com/FactomProject/fctwallet/Wallet/Utility"
 	"github.com/FactomProject/fctwallet/Wallet"
+	"github.com/FactomProject/fctwallet/Wallet/Utility"
+	"github.com/hoisie/web"
 )
 
 var _ = fct.Address{}
@@ -35,8 +35,56 @@ func HandleFactoidGenerateECAddress(ctx *web.Context, name string) {
 		reportResults(ctx, "Name provided is not valid", false)
 		return
 	}
-	
+
 	adrstr, err := Wallet.GenerateECAddressString(name)
+	if err != nil {
+		reportResults(ctx, err.Error(), false)
+		return
+	}
+
+	reportResults(ctx, adrstr, true)
+}
+
+func HandleFactoidGenerateAddressFromPrivateKey(ctx *web.Context, name string, privateKey string) {
+	ok := Utility.IsValidKey(name)
+	if !ok {
+		reportResults(ctx, "Name provided is not valid", false)
+		return
+	}
+	if len(privateKey) != 64 && len(privateKey) != 128 {
+		reportResults(ctx, "Invalid private key length", false)
+		return
+	}
+	if Utility.IsValidHex(privateKey) == false {
+		reportResults(ctx, "Invalid private key format", false)
+		return
+	}
+
+	adrstr, err := Wallet.GenerateAddressStringFromPrivateKey(name, privateKey)
+	if err != nil {
+		reportResults(ctx, err.Error(), false)
+		return
+	}
+
+	reportResults(ctx, adrstr, true)
+}
+
+func HandleFactoidGenerateECAddressFromPrivateKey(ctx *web.Context, name string, privateKey string) {
+	ok := Utility.IsValidKey(name)
+	if !ok {
+		reportResults(ctx, "Name provided is not valid", false)
+		return
+	}
+	if len(privateKey) != 64 && len(privateKey) != 128 {
+		reportResults(ctx, "Invalid private key length", false)
+		return
+	}
+	if Utility.IsValidHex(privateKey) == false {
+		reportResults(ctx, "Invalid private key format", false)
+		return
+	}
+
+	adrstr, err := Wallet.GenerateECAddressStringFromPrivateKey(name, privateKey)
 	if err != nil {
 		reportResults(ctx, err.Error(), false)
 		return
