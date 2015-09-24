9451c76ceba993f671bc5f4a759cd6292866e64a
Paul Snow <snow.paul@gmail.com>
2015-09-24 11:24:34 -0500

    Added a simple listing of transactions gobally, and by address to the walletapp.  Will add the cli support shortly.

--- list.go
+++ list.go
@@ -5,114 +5,11 @@ package main
 
 
 import (
-	"bytes"
 	"encoding/hex"
 	"fmt"
-	"github.com/FactomProject/factoid/block"
-	"github.com/FactomProject/factom"
-	"github.com/FactomProject/FactomCode/common"
+	"github.com/FactomProject/fctwallet/Wallet/Utility"
 )
 
-
-/************************************************
- * Transaction listing code
- ***********************************************/
-
-// Older blocks smaller indexes.  All the Factoid Directory blocks
-var DirectoryBlocks  = make([]*common.DirectoryBlock,0,100)
-var FactoidBlocks    = make([]block.IFBlock,0,100)
-var DBHead    []byte
-var DBHeadStr string = ""
-
-// Refresh the Directory Block Head.  If it has changed, return true.
-// Otherwise return false.
-func getDBHead() bool {
-	db, err := factom.GetDBlockHead()
-	
-	if err != nil {
-		panic(err.Error())
-	}
-	
-	if db.KeyMR != DBHeadStr {
-		DBHeadStr = db.KeyMR
-		DBHead,err = hex.DecodeString(db.KeyMR)
-		if err != nil {
-			panic(err.Error())
-		}
-		
-		return true
-	}
-	return false
-}
-
-func getAll() error {
-	dbs := make([] *common.DirectoryBlock,0,100)
-	next := DBHeadStr
-	
-	for {
-		blk,err := factom.GetRaw(next)
-		if err != nil {
-			panic(err.Error())
-		}
-		db := new(common.DirectoryBlock)
-		err = db.UnmarshalBinary(blk)
-		if err != nil {
-			panic(err.Error())
-		}
-		dbs = append(dbs,db)
-		if bytes.Equal(db.Header.PrevKeyMR.Bytes(),common.ZERO_HASH[:]) {
-			break
-		}
-		next = hex.EncodeToString(db.Header.PrevKeyMR.Bytes())
-	}
-	
-	fmt.Println(len(dbs))
-	
-	for i:= len(dbs)-1;i>=0; i-- {
-		DirectoryBlocks = append(DirectoryBlocks,dbs[i])
-		fb := new(block.FBlock)
-		for _,dbe := range dbs[i].DBEntries {
-			if bytes.Equal(dbe.ChainID.Bytes(),common.FACTOID_CHAINID) {
-				hashstr := hex.EncodeToString(dbe.KeyMR.Bytes())
-				fdata,err := factom.GetRaw(hashstr)
-				if err != nil {
-					panic(err.Error())
-				}
-				err = fb.UnmarshalBinary(fdata)
-				if err != nil {
-					panic(err.Error())
-				}
-				FactoidBlocks = append(FactoidBlocks,fb)
-				break
-			}
-		}
-		if fb == nil {
-			fmt.Println("Missing Factoid Block")
-		}
-	}
-	return nil
-}
-
-func refresh() error {
-	if DBHead == nil {
-		getDBHead()
-		getAll()
-	}
-	if getDBHead() {
-		
-	}
-	return nil
-}
-
-func DumpTransactions() error {
-	if err := refresh(); err != nil {
-		fmt.Println(err)
-		return err
-	}
-	
-	fmt.Println("Factoid blocks: ",len(FactoidBlocks))
-	return nil
-}
 /************************************************************
  * List
  ************************************************************/
@@ -124,15 +21,46 @@ type List struct {
 var _ ICommand = (*List)(nil)
 
 // List transactions <address list> 
-func (List) Execute(state IState, args []string) error {
+func (List) Execute(state IState, args []string) (err error) {
 	if len(args) <= 1 {
 		return fmt.Errorf("Nothing to list")
 	}
 	switch args[1] {
 		case "transactions" :
-			DumpTransactions()
+			if len(args)==2 {
+				fmt.Println("Listing all transactions: ")
+				var list []byte
+				if list, err = Utility.DumpTransactions(nil); err != nil {
+					return err
+				}
+				fmt.Print(string(list))
+				break
+			}else{
+				var addresses [][]byte
+				var adr string
+				for i := 2; i < len(args); i++ {
+					adr, err = LookupAddress(state, "FA",args[i])
+					if err != nil {
+						adr, err = LookupAddress(state, "EC",args[i])
+						if err != nil {
+							return fmt.Errorf("Could not understand address %s",args[i])
+						}
+					}
+					badr,err := hex.DecodeString(adr)
+					if err != nil {
+						return fmt.Errorf("Could not understand address %s",args[i])
+					}
+					addresses = append(addresses,badr)
+				}
+				var list []byte
+				if list, err = Utility.DumpTransactions(addresses); err != nil {
+					return err
+				}
+				fmt.Print(string(list))
+			}
+				
 		default :
-			fmt.Println("Don't understand '%s'",args[1])
+			fmt.Printf("Don't understand '%s'",args[1])
 	}
 	return nil
 }
